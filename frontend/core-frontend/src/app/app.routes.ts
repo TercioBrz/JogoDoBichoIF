fb03d5f91279c49047b40416da5559c2a142df7f
tercio.brelaz <tercio.brelaz@fpf.br>
2026-02-19 17:47:55 -0300

    imp[19/02/25] adicionando a pipe do front

--- frontend/core-frontend/src/app/app.routes.ts
+++ frontend/core-frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    { 
+        path: 'login', 
+        loadComponent: () => 
+            import("./telalogin/telalogin").then(m => m.Telalogin) 
+    },
+
+    {
+        path: '',
+        loadComponent: () =>
+            import("./home/home").then(m => m.Home)
+        // children: [
+
+        //     {
+                
+        //     }
+
+        // ]
+    }
+    
+
+];
